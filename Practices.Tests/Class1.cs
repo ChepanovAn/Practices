8ea59308b1b735c29c0f024847d6ea5be75350b8
Анатолий <Chepanov199287@gmail.com>
2023-06-29 20:32:11 +0300

    Добавьте файлы проекта.

---
+++ Practices.Tests/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Practices;
+
+
+namespace Practices.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AdditionalTest()
+        {
+            var calculator = new Calculator();
+            Assert.IsTrue(calculator.Additional(10, 10) == 20);
+        }
+
+        [TestMethod()]
+        public void SubtractionTest()
+        {
+            var calculator = new Calculator();
+            Assert.IsTrue(calculator.Subtraction(10, 10) == 0);
+        }
+
+        [TestMethod()]
+        public void MiltiplicationTest()
+        {
+            var calculator = new Calculator();
+            Assert.IsTrue(calculator.Miltiplication(10, 10) == 100);
+        }
+
+        [TestMethod()]
+        public void DivisionTest()
+        {
+            var calculator = new Calculator();
+            Assert.IsTrue(calculator.Division(10, 10) == 1);
+        }
+    }
+}
